1ac815ccb26f17e181bc0be38f0053f351e08f78
Philippe Coval <p.coval@samsung.com>
2018-08-30 14:47:32 +0200

    WIP: mod: example/mastodon-thing/mastodon-thing.js (sandbox/rzr/iotjs/devel/master)
    Change-Id: Ice3fa3fb5d25711d9c1295ccdd9cde7a53e3e410
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/mastodon-thing/mastodon-thing.js
+++ example/mastodon-thing/mastodon-thing.js
@@ -9,7 +9,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
-const webthing = require('webthing');
+let webthing;
+
+try {
+  webthing = require('../webthing');
+} catch (err) {
+  webthing = require('webthing');
+}
 
 const Property = webthing.Property;
 const SingleThing = webthing.server.SingleThing;
